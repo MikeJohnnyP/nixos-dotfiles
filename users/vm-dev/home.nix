ce21883e7aec2c60299ad07e15eda525717f8203
MikeJohnnyP <tahoangphuc1901@gmail.com>
2025-12-13 00:38:52 +0700

    Fix another commit

--- users/vm-dev/home.nix
+++ users/vm-dev/home.nix
@@ -1,57 +1,55 @@
 { config, pkgs, ... }:
 
 {
-	imports = [
-		./../../modules/home-manager/default.nix
-		./../../modules/home-manager/i3-wm.nix
-		./../../modules/home-manager/firefox.nix
-		./../../modules/home-manager/zathura.nix
-		./../../modules/home-manager/vscode.nix
-		./../../modules/home-manager/neovim.nix
-		./../../modules/home-manager/picom.nix
-		./lang.nix
-  	];
+  imports = [
+    ./../../modules/home-manager/default.nix
+    ./../../modules/home-manager/i3-wm.nix
+    ./../../modules/home-manager/firefox.nix
+    ./../../modules/home-manager/zathura.nix
+    ./../../modules/home-manager/vscode.nix
+    ./../../modules/home-manager/neovim.nix
+    ./../../modules/home-manager/picom.nix
+    ./lang.nix
+  ];
 
-	home.username = "mikejohnp";
-	home.homeDirectory = "/home/mikejohnp";
-	programs.git = {
-		enable = true;
-		userName = "MikeJohnP";
-		userEmail = "tahoangphuc1901@gmail.com";
-    extraConfig = {
-		init.defaultBranch = "main";
-		pull.rebase = true;
-		core.editor = "vi";
+  home.username = "mikejohnp";
+  home.homeDirectory = "/home/mikejohnp";
+  programs.git = {
+    enable = true;
+    settings = {
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      core.editor = "vim";
     };
   };
-	home.stateVersion = "25.05";
+  home.stateVersion = "25.05";
 
-	within.zsh.enable = true;
-	within.ghostty.enable = true;
-	within.neovim.enable = true;
-	within.alacritty.enable = true;
+  within.zsh.enable = true;
+  within.ghostty.enable = true;
+  within.neovim.enable = true;
+  within.alacritty.enable = true;
 
-	nixpkgs.config.allowUnfree = true;
-	
-	# home.file.".config/nvim".source = ../../config/nvim;
+  nixpkgs.config.allowUnfree = true;
 
-	home.packages = with pkgs; [
-		ripgrep
-		nil
-		nixpkgs-fmt
-		alacritty
-		ghostty
-		fastfetch
-		lazygit
-		dmenu
-		feh
-		nerd-fonts.jetbrains-mono
-	];
+  # home.file.".config/nvim".source = ../../config/nvim;
 
-  	home.sessionVariables = {
-    	EDITOR = "nvim";
-  	};
+  home.packages = with pkgs; [
+    ripgrep
+    nil
+    nixpkgs-fmt
+    alacritty
+    ghostty
+    fastfetch
+    lazygit
+    dmenu
+    feh
+    nerd-fonts.jetbrains-mono
+  ];
 
-	# Let Home Manager install and manage itself.
-	programs.home-manager.enable = true;
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
