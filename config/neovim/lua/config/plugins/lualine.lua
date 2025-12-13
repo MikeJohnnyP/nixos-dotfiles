875611c3bcbeee69a27f785f25a962d5822806c8
MikeJohnnyP <tahoangphuc1901@gmail.com>
2025-12-13 22:16:35 +0700

    Remove buffers in inactive mode lualine

--- config/neovim/lua/config/plugins/lualine.lua
+++ config/neovim/lua/config/plugins/lualine.lua
@@ -69,7 +69,7 @@ return {
 			inactive_sections = {
 				lualine_a = {},
 				lualine_b = {},
-				lualine_c = { { "filename", path = 1 }, "buffers" },
+				lualine_c = { { "filename", path = 1 } },
 				lualine_x = { { "location", padding = 0 } },
 				lualine_y = {},
 				lualine_z = {},
