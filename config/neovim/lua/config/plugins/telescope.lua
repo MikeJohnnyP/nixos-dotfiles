a5cc91d2458d44badce3db0c50f31a0a347e86a6
MikeJohnnyP <tahoangphuc1901@gmail.com>
2025-12-17 02:10:26 +0700

    Added vue into treesitter, ignore node_modules from telescope

--- config/neovim/lua/config/plugins/telescope.lua
+++ config/neovim/lua/config/plugins/telescope.lua
@@ -22,6 +22,11 @@ return {
 				local actions = require("telescope.actions")
 
 				require("telescope").setup({
+					defaults = {
+						file_ignore_patterns = {
+							"^node_modules/",
+						},
+					},
 					-- use ui-select dropdown as our ui
 					extensions = {
 						["ui-select"] = {
--- config/neovim/lua/config/plugins/treesitter.lua
+++ config/neovim/lua/config/plugins/treesitter.lua
@@ -26,6 +26,7 @@ return {
 					"bash",
 					"php",
 					"blade",
+					"vue",
 				},
 				auto_install = false,
 				highlight = {
