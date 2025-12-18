79b86df04cb9fae1f990e780b2f40c7b729ce098
MikeJohnnyP <tahoangphuc1901@gmail.com>
2025-12-18 21:43:37 +0700

    Added copilot chat

--- config/neovim/lua/config/plugins/completion.lua
+++ config/neovim/lua/config/plugins/completion.lua
@@ -1,6 +1,25 @@
 return {
 	{
-		"github/copilot.vim",
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		keys = {
+			{ "<leader>zc", "<cmd>CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
+			{ "<leader>ze", "<cmd>CopilotChatExplain<CR>", mode = "v", desc = "Explain Code" },
+			{ "<leader>zr", "<cmd>CopilotChatReview<CR>", mode = "v", desc = "Review Code" },
+			{ "<leader>zf", "<cmd>CopilotChatFix<CR>", mode = "v", desc = "Fix Code Issues" },
+			{ "<leader>zo", "<cmd>CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code" },
+			{ "<leader>zd", "<cmd>CopilotChatDocs<CR>", mode = "v", desc = "Generate Docs" },
+			{ "<leader>zt", "<cmd>CopilotChatTests<CR>", mode = "v", desc = "Generate Tests" },
+			{ "<leader>zm", "<cmd>CopilotChatCommit<CR>", mode = "n", desc = "Generate Commit Message" },
+			{ "<leader>zs", "<cmd>CopilotChatCommit<CR>", mode = "v", desc = "Generate Commit for Selection" },
+		},
 	},
 	{
 		"L3MON4D3/LuaSnip",
